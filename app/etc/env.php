9a6f18b8f930fd55155bd93197b2b728ad0d4d6a
Le Cong Tien <92415235+congtiendev@users.noreply.github.com>
2023-09-14 11:12:37 +0700

    Update env.php

--- app/etc/env.php
+++ app/etc/env.php
@@ -1,5 +1,3 @@
-  GNU nano 3.2                                                               env.php                                                                          
-                                                                                                                                                              
 <?php
 return array (
   'backend' =>
