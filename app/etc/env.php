1a082d40909104e96fef0dee3bbb5508aec43754
Le Cong Tien <92415235+congtiendev@users.noreply.github.com>
2023-09-14 10:58:19 +0700

    Update env.php

--- app/etc/env.php
+++ app/etc/env.php
@@ -1,85 +1,67 @@
+  GNU nano 3.2                                                               env.php                                                                          
+                                                                                                                                                              
 <?php
-return [
-    'backend' => [
-        'frontName' => 'admin'
-    ],
-    'cache' => [
-        'graphql' => [
-            'id_salt' => 'DI6dUXIExDOsihyvz36uMk2iaKdLMgkY'
-        ],
-        'frontend' => [
-            'default' => [
-                'id_prefix' => 'bbd_'
-            ],
-            'page_cache' => [
-                'id_prefix' => 'bbd_'
-            ]
-        ],
-        'allow_parallel_generation' => false
-    ],
-    'remote_storage' => [
-        'driver' => 'file'
-    ],
-    'queue' => [
-        'consumers_wait_for_messages' => 1
-    ],
-    'crypt' => [
-        'key' => '28a950a496e2954284cc0dd5161962a0'
-    ],
-    'db' => [
-        'table_prefix' => '',
-        'connection' => [
-            'default' => [
-                'host' => 'localhost',
-                'dbname' => 'wjhnrcafyz',
-                'username' => 'wjhnrcafyz',
-                'password' => '@722003Xx',
-                'model' => 'mysql4',
-                'engine' => 'innodb',
-                'initStatements' => 'SET NAMES utf8;',
-                'active' => '1',
-                'driver_options' => [
-                    1014 => false
-                ]
-            ]
-        ]
-    ],
-    'resource' => [
-        'default_setup' => [
-            'connection' => 'default'
-        ]
-    ],
+return array (
+  'backend' =>
+  array (
+    'frontName' => 'admin',
+  ),                                                                                                                                                          
+  'install' =>
+  array (
+    'date' => 'Thu, 14 Sep 2023 03:41:23 +0000',
+  ),                                                                                                                                                          
+  'crypt' =>
+  array (
+    'key' => 'm1ToGGTvSbJVL00zgNyqyMpmP9Uo1mCd',
+  ),                                                                                                                                                          
+  'session' =>
+  array (
+    'save' => 'files',
+  ),                                                                                                                                                          
+  'db' =>
+  array (
+    'table_prefix' => '',
+    'connection' =>
+    array (
+      'default' =>
+      array (
+        'host' => 'localhost',
+        'dbname' => 'dppytvkwjg',
+        'username' => 'dppytvkwjg',
+        'password' => 'x5MUA7JAXF',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+        ),
+  ),                                                                                                                                                          
+  'resource' =>
+  array (
+    'default_setup' =>
+    array (
+      'connection' => 'default',
+    ),                                                                                                                                                        
+  ),     
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'developer',
-    'session' => [
-        'save' => 'files'
-    ],
-    'lock' => [
-        'provider' => 'db'
-    ],
-    'directories' => [
-        'document_root_is_pub' => true
-    ],
-    'cache_types' => [
-        'config' => 1,
-        'layout' => 1,
-        'block_html' => 1,
-        'collections' => 1,
-        'reflection' => 1,
-        'db_ddl' => 1,
-        'compiled_config' => 1,
-        'eav' => 1,
-        'customer_notification' => 1,
-        'config_integration' => 1,
-        'config_integration_api' => 1,
-        'full_page' => 1,
-        'config_webservice' => 1,
-        'translate' => 1
-    ],
-    'downloadable_domains' => [
-        'trainningplus.loc'
-    ],
-    'install' => [
-        'date' => 'Wed, 13 Sep 2023 07:01:38 +0000'
-    ]
-];
+  'MAGE_MODE' => 'default',
+  'cache_types' =>
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),                                                                                                                                                          
+);                                                                                                                                                            
+                  
+                                                 
+
+
